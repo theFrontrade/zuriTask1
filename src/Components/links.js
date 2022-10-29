ed739527a6a10d67553ec0891da146ac93285c4e
theFrontrade <sadeniyi016@gmail.com>
2022-10-29 06:26:19 +0100

    Update links.js

--- src/Components/links.js
+++ src/Components/links.js
@@ -8,7 +8,8 @@ const Links = () => {
           target='_blank'
           rel='noreferrer'
           className='link-button'
-          key={item.id}
+          key={item.key}
+          id={item.id}
         >
           {" "}
           {item.name}
@@ -37,31 +38,37 @@ export default Links;
 
 export const linkList = [
   {
+    key: 1,
     id: 'twitter__link',
     name: 'Twitter link',
     link: 'https://twitter.com/theFrontrade',
   },
   {
+    key: 2,
     id: 'btn__zuri',
     name: 'Zuri Team',
     link: 'https://training.zuri.team/',
   },
   {
+    key: 3,
     id: 'books',
     name: 'Zuri Books',
     link: 'https://books.zuri.team/',
   },
   {
+    key: 4,
     id: 'books__python',
     name: 'Python Books',
     link: 'https://books.zuri.team/python-for-beginners?ref_id=samueladeniyi',
   },
   {
+    key: 5,
     id: 'pitch',
     name: 'Background Checks for Codes',
     link: 'https://background.zuri.team/',
   },
   {
+    key: 6,
     id: 'books__design',
     name: 'Design Books',
     link: 'https://books.zuri.team/design-rules',
